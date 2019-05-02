07ce27e683106de8d69ecb651ccc846658dfe58c
Vlad Neculai <vladut.neculai@yahoo.com>
2019-05-02 20:24:23 +0200

    Removed Auth

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const auth = require('../middleware/auth')
 const multer = require('../middleware/multer-config')
 
 const postCtrl = require('../controllers/posts');
